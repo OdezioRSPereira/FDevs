d0838356c89e81d2a368905c9ad3802b9bc5991b
odezio.pereira <odezio.pereira@NOTEBOOK96>
2021-05-08 10:00:22 -0300

    Adicionando arquivos de dados usuarios e quizzes

---
+++ FDevsQuiz/Controllers/UsuariosController.cs
@@ -0,0 +1,26 @@
+﻿using FDevsQuiz.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace FDevsQuiz.Controllers
+{
+    [Controller]
+    [Route(("usuarios"))]
+    public class UsuariosController : ControllerBase
+    {
+
+        private ICollection<T> CarregarDados<T>()
+        {
+            using var openStream = System.IO.File.OpenRead(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "usuarios.json"));
+            return JsonSerializer.DeserializeAsync<ICollection<T>>(openStream, new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            }).Result;
+        }
+
+    }
+}
